7f9673b73d1cefb6d6cd5afd589bc625a9c013c1
jeongseokhyeon <jeong9235@gmail.com>
2025-12-10 16:37:20 +0900

    개발 초기 세팅

---
+++ src/main/java/com/library/bookarte/BookarteApplication.java
@@ -0,0 +1,13 @@
+package com.library.bookarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookarteApplication.class, args);
+	}
+
+}
