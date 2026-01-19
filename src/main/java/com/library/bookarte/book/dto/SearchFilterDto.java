b13bf93bb9987dc115efcf03018e9b6b9d8f00e6
jeongseokhyeon <jeong9235@gmail.com>
2026-01-19 20:36:38 +0900

    feat:저자 검색, 출판일 기간 검색 기능 추가

--- src/main/java/com/library/bookarte/book/dto/SearchFilterDto.java
+++ src/main/java/com/library/bookarte/book/dto/SearchFilterDto.java
@@ -1,7 +1,10 @@
 package com.library.bookarte.book.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
@@ -11,4 +14,12 @@ public class SearchFilterDto {
     private final String category;
     private final String isbn;
     private final String publisherName;
+    private final String author;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private final LocalDate publicationDateStart;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private final LocalDate publicationDateEnd;
+
 }
--- src/main/java/com/library/bookarte/book/repository/BookRepositoryCustomImpl.java
+++ src/main/java/com/library/bookarte/book/repository/BookRepositoryCustomImpl.java
@@ -3,12 +3,14 @@ package com.library.bookarte.book.repository;
 import com.library.bookarte.book.dto.BookResDto;
 import com.library.bookarte.book.dto.SearchFilterDto;
 import com.library.bookarte.book.entity.Book;
+import com.library.bookarte.book.entity.type.ParticipantType;
 import com.querydsl.core.group.GroupBy;
 import com.querydsl.core.group.GroupBy.*;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -16,6 +18,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,6 +38,9 @@ public class BookRepositoryCustomImpl implements BookRepositoryCustom {
         String bookTitle = searchFilterDto.getBookTitle();
         String bookIsbn = searchFilterDto.getIsbn();
         String publisherName = searchFilterDto.getPublisherName();
+        String author = searchFilterDto.getAuthor();
+        LocalDate start = searchFilterDto.getPublicationDateStart();
+        LocalDate end = searchFilterDto.getPublicationDateEnd();
 
         //도서 id만 선 조회
         List<Long> ids = jpaQueryFactory
@@ -45,7 +51,9 @@ public class BookRepositoryCustomImpl implements BookRepositoryCustom {
                         categoryNameEq(categoryName),
                         titleContains(bookTitle),
                         isbnContains(bookIsbn),
-                        publisherContains(publisherName)
+                        publisherContains(publisherName),
+                        authorContains(author),
+                        publicationDateBetween(start,end)
                 )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
@@ -83,28 +91,60 @@ public class BookRepositoryCustomImpl implements BookRepositoryCustom {
 
     // ===== 조건 메서드 =====
 
+    //카테고리 조건 메서드
     private BooleanExpression categoryNameEq(String categoryName) {
         return StringUtils.hasText(categoryName)
                 ? book.category.categoryName.eq(categoryName)
                 : null;
     }
-
+    //도서 제목 조건 메서드
     private BooleanExpression titleContains(String bookTitle) {
         return StringUtils.hasText(bookTitle)
                 ? book.bookTitle.contains(bookTitle)
                 : null;
     }
 
+    //isbn 조건 메서드
     private BooleanExpression isbnContains(String isbn) {
         return isbn != null
                 ? book.bookIsbn.contains(isbn)
                 : null;
     }
 
+    //출판사 조건 메서드
     private BooleanExpression publisherContains(String publisherName) {
         return StringUtils.hasText(publisherName)
                 ? book.publisherName.contains(publisherName)
                 : null;
     }
 
+    private BooleanExpression authorContains(String author){
+        if(!StringUtils.hasText(author)) {
+            return null;
+        }
+        return book.participants.any().name.contains(author)
+                .and(book.participants.any().type.eq(ParticipantType.AUTHOR));
+    }
+
+    //특정 날짜 이후 출판일 조건 메서드 (publicationDate >= start)
+    private BooleanExpression publicationDateGoe(LocalDate start) {
+        return start != null ? book.publicationDate.goe(start) : null;
+    }
+
+    //특정 날짜 이전 출판일 조건 메서드 (publicationDate <= start)
+    private BooleanExpression publicationDateLoe(LocalDate end) {
+        return end != null ? book.publicationDate.loe(end) : null;
+    }
+
+
+    private BooleanExpression publicationDateBetween(LocalDate start, LocalDate end ){
+        BooleanExpression isGoe = publicationDateGoe(start);
+        BooleanExpression isLoe = publicationDateLoe(end);
+
+        if (isGoe != null && isLoe != null) return isGoe.and(isLoe);
+        if (isGoe != null) return isGoe;
+        if (isLoe != null) return isLoe;
+
+        return null;
+    }
 }
