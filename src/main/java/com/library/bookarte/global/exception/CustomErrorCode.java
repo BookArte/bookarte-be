3856b9b2d313b6e67da7fba35139ac5a081b8f3c
ngm0228 <ngm0228@gmail.com>
2026-01-22 15:39:43 +0900

    fix: 커스텀 에러코드 에러수정

--- src/main/java/com/library/bookarte/global/exception/CustomErrorCode.java
+++ src/main/java/com/library/bookarte/global/exception/CustomErrorCode.java
@@ -19,7 +19,7 @@ public enum CustomErrorCode {
     //auth
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원정보가 존재하지 않습니다."),
     INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다."),
-    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않는 토큰입니다.");
+    INVALID_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않는 토큰입니다."),
   
     //recommendation
     RECOMMENDATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 추천 도서는 존재하지 않습니다"),
