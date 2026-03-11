b05b5d04bc6942bb652977c3937c7d47cf582f81
ngm0228 <ngm0228@gmail.com>
2026-03-11 20:57:54 +0900

    chore: aws 파일 업로드 확장자 블랙리스트 추가 및 용량 제한

--- src/main/java/com/library/bookarte/global/util/S3Service.java
+++ src/main/java/com/library/bookarte/global/util/S3Service.java
@@ -1,6 +1,5 @@
 package com.library.bookarte.global.util;
 
-import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -10,6 +9,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,12 +21,28 @@ public class S3Service {
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
+    @Value("${file.upload.forbidden-extensions}")
+    private List<String> forbiddenExtensions;
+
+    private static final long MAX_FILE_SIZE = 10 * 1024 * 1024;
+
     public String uploadFile(MultipartFile file) {
+
+        if (file.getSize() > MAX_FILE_SIZE) {
+            throw new RuntimeException("파일 용량은 10MB를 초과할 수 없습니다.");
+        }
+
         String originalFilename = file.getOriginalFilename();
         String extension = "";
+
         if (originalFilename != null && originalFilename.contains(".")) {
-            extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            extension = originalFilename.substring(originalFilename.lastIndexOf(".")).toLowerCase();
         }
+
+        if (forbiddenExtensions.contains(extension)) {
+            throw new RuntimeException("업로드가 금지된 파일 형식입니다: " + extension);
+        }
+
         String s3FileName = UUID.randomUUID().toString() + extension;
 
         try {
