5cb6d53faeec98f8c1c530f493bd194adc3cf454
ngm0228 <ngm0228@gmail.com>
2026-03-25 19:48:51 +0900

    fix: 세미콜론(;) 콤마(,)로 수정

--- src/main/java/com/library/bookarte/global/exception/CustomErrorCode.java
+++ src/main/java/com/library/bookarte/global/exception/CustomErrorCode.java
@@ -56,7 +56,7 @@ public enum CustomErrorCode {
 
     //board
     BOARD_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시판입니다."),
-    INVALID_BOARD_TYPE(HttpStatus.BAD_REQUEST, "잘못된 타입입니다.");
+    INVALID_BOARD_TYPE(HttpStatus.BAD_REQUEST, "잘못된 타입입니다."),
     //wish
     WISH_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 관심 도서는 존재하지 않습니다");
 
