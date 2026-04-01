2304a8364682c1ebbec162de94dedbfffc60600c
jeongseokhyeon <jeong9235@gmail.com>
2026-04-01 18:49:41 +0900

    refactor: 배치 설정 코드 분리

---
+++ src/main/java/com/library/bookarte/global/config/batch/LibararyBatchJobConfig.java
@@ -0,0 +1,30 @@
+package com.library.bookarte.global.config.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class LibararyBatchJobConfig {
+    private final JobRepository jobRepository;
+
+    /**
+     * 전체 도서관 관리 마스터 배치
+     */
+    @Bean
+    public Job libraryMasterJob(
+            @Qualifier("overdueCheckStep") Step overdueCheckStep,
+            @Qualifier("popularBookStatStep") Step popularBookStatStep
+    ) {
+        return new JobBuilder("libraryMasterJob", jobRepository)
+                .start(overdueCheckStep)      // 1. 연체 처리
+                .next(popularBookStatStep)   // 2. 인기 도서 집계
+                .build();
+    }
+}
