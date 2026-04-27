7f2dfaf8f066d5a9927dc031d463d424e5b739a9
jeongseokhyeon <jeong9235@gmail.com>
2026-04-27 15:03:02 +0900

    test: 테스트용 더미 회원 데이터 생성 코드

--- src/test/java/com/library/bookarte/support/DataAdHocGenerator.java
+++ src/test/java/com/library/bookarte/support/DataAdHocGenerator.java
@@ -1,6 +1,8 @@
 package com.library.bookarte.support;
 
 import com.library.bookarte.global.config.BulkDataGenerator;
+import com.library.bookarte.member.entity.Member;
+import com.library.bookarte.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +11,10 @@ import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 public class DataAdHocGenerator {
 
     @Autowired private BulkDataGenerator bulkDataGenerator;
+    @Autowired private MemberRepository memberRepository;
 
     @Test
     @Commit // 테스트가 성공하면 롤백하지 않고 DB에 반영
@@ -28,5 +30,17 @@ public class DataAdHocGenerator {
         System.out.println("성공적으로 " + totalBooks + "건의 데이터를 적재했습니다.");
         System.out.println("소요 시간: " + (endTime - startTime) + "ms");
         System.out.println("------------------------------------------");
+    }
+
+    @Test
+    @Commit
+    @DisplayName("로컬 DB 테스트 유저 100건 데이터 적재")
+    void generateUserDate() {
+        int totalUsers =  100;
+
+        for (int i = 1; i <= totalUsers; i++){
+            Member member = FixtureFactory.createMember("test" + i);
+            memberRepository.save(member);
+        }
     }
 }
