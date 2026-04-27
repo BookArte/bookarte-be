4cd98453dbae5a887ae9eb7cd0ed373d831f15f6
jeongseokhyeon <jeong9235@gmail.com>
2026-04-27 15:15:38 +0900

    chore: 불필요한 import문 삭제

--- src/main/java/com/library/bookarte/member/controller/MemberControllerDocs.java
+++ src/main/java/com/library/bookarte/member/controller/MemberControllerDocs.java
@@ -9,12 +9,9 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Tag(name = "Member")
 public interface MemberControllerDocs {
